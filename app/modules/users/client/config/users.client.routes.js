42e7169a18ebbfb9b3f07e47fd50f07d2abc97c3
krystalmejia24 <krystalmejia24@ufl.edu>
2016-02-07 21:27:10 -0500

    fixed to work with authentication view

--- app/modules/users/client/config/users.client.routes.js
+++ app/modules/users/client/config/users.client.routes.js
@@ -21,14 +21,6 @@ angular.module('users').config(['$stateProvider',
         url: '/password',
         templateUrl: 'modules/users/client/views/settings/change-password.client.view.html'
       })
-      .state('settings.accounts', {
-        url: '/accounts',
-        templateUrl: 'modules/users/client/views/settings/manage-social-accounts.client.view.html'
-      })
-      .state('settings.picture', {
-        url: '/picture',
-        templateUrl: 'modules/users/client/views/settings/change-profile-picture.client.view.html'
-      })
       .state('authentication', {
         abstract: true,
         url: '/authentication',
