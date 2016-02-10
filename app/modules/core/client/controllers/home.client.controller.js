f1f19071c89fa31e73750869786fa38299f411b3
krystalmejia24 <krystalmejia24@ufl.edu>
2016-02-10 14:53:17 -0500

    drew simple polygon, commented how

--- app/modules/core/client/controllers/home.client.controller.js
+++ app/modules/core/client/controllers/home.client.controller.js
@@ -14,6 +14,28 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
         This is a simple rendering of our map. 
     */
+
+
+    //these coordinates were used to draw the polygon below
+    var polygonExample = {
+        Waldo: {
+            lat: 29.7897,
+            lng: -82.1708
+        },
+        LaCrosse: {
+            lat: 29.8500,
+            lng: -82.4000
+        },
+        Micanopy: {
+            lat: 29.5064,
+            lng: -82.2819
+        },
+        Al: {
+            lat: 29.7792,
+            lng: -82.4797
+        }
+    };
+
 	angular.extend($scope, {
 		alachua: {
 			//lat: 29.6520,
@@ -25,7 +47,23 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     		fullscreen: {
     			position: 'topleft'
     		}
-    	}
+        },
+        /*
+        This is an example of how to draw polygons on the map
+        */
+        paths: {
+            polygon: {
+                type: "polygon",
+                latlngs: [
+                    polygonExample.Waldo,
+                    polygonExample.LaCrosse,
+                    polygonExample.Al,
+                    polygonExample.Micanopy
+                ],
+                color: 'red',
+                fillColor: 'red'
+            }
+        }
     });
 	}
 ]);
