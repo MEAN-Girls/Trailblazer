bcbc37d1b652956666e50102251d86da70312f7f
krystalmejia24 <krystalmejia24@ufl.edu>
2016-02-10 01:06:02 -0500

    added some leaflet plugins. Home page now renders map

--- app/modules/core/client/controllers/home.client.controller.js
+++ app/modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,20 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
-  }
+
+    // center map
+	angular.extend($scope, {
+		alachua: {
+			//lat: 29.6520,
+			//lng: -82.3250,
+			zoom: 10,
+			autoDiscover: true
+    	},
+    	controls: {
+    		fullscreen: {
+    			position: 'topleft'
+    		}
+    	}
+    });
+	}
 ]);
