4e7aba1253c2d8524527654a97ebe13318f01a01
krystalmejia24 <krystalmejia24@ufl.edu>
2016-02-10 13:43:37 -0500

    added comments

--- app/modules/core/client/controllers/home.client.controller.js
+++ app/modules/core/client/controllers/home.client.controller.js
@@ -5,7 +5,15 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     // This provides Authentication context.
     $scope.authentication = Authentication;
 
-    // center map
+    /*
+        Some quick references for leaflet use in the docs, 
+
+        L.Class = angular module. whenever you want to add some functionality
+
+        Later on we will be adding factories to work with new classes.
+
+        This is a simple rendering of our map. 
+    */
 	angular.extend($scope, {
 		alachua: {
 			//lat: 29.6520,
